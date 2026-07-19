74032475211a9d2b5412bb271e116423d978c2c3
SKH09 <123449604+SKH09@users.noreply.github.com>
2026-07-19 17:34:26 +0530

    deleted the appspec, start and stop files

--- CodeBuild/sample-python-app/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the latest image
-docker pull skh09/simple-python-flask-app
-
-# Run the container with a specific name so we can stop it later
-docker run -d -p 8000:8000 --name simple-python-flask-app skh09/simple-python-flask-app
--- CodeBuild/sample-python-app/stop_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# Stop and remove the old container if it exists, ignoring errors if it's not running yet
-docker stop simple-python-flask-app || true
-docker rm simple-python-flask-app || true

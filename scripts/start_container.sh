58baf5e3fc486ac9896ad8de29a0f0202a572aa4
SKH09 <123449604+SKH09@users.noreply.github.com>
2026-07-20 13:24:58 +0530

    changed the app name

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the latest image
-docker pull skh09/simple-python-flask-app:latest
+docker pull skh09/simple-python-app:latest
 
 # Run the container with a specific name so we can stop it later
-docker run -d -p 8000:8000 --name simple-python-flask-app skh09/simple-python-flask-app:latest
+docker run -d -p 8000:8000 --name simple-python-flask-app skh09/simple-python-app:latest

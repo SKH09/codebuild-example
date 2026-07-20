45561da75478fbb0a256f77a8b7398cb504449d0
SKH09 <123449604+SKH09@users.noreply.github.com>
2026-07-20 11:36:14 +0530

    modified the start and stop files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the latest image
-docker pull skh09/simple-python-flask-app
+docker pull skh09/simple-python-flask-app:latest
 
 # Run the container with a specific name so we can stop it later
-docker run -d -p 8000:8000 --name simple-python-flask-app skh09/simple-python-flask-app
+docker run -d -p 8000:8000 --name simple-python-flask-app skh09/simple-python-flask-app:latest
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-# Stop and remove existing container if running
+# Force delete any running container named simple-python-flask-app
 docker rm -f simple-python-flask-app || true
